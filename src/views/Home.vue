d85399fc35104a772ad558612c2c7c6b677d803e
RoB8080 <75566977@qq.com>
2019-06-27 23:59:36 +0800

    Update - Delete useless icon on desktop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,10 +28,10 @@
             return {
                 items: [
                     {icon: "el-icon-setting", label:"设置", route:"/setting"},
-                    {icon: "el-icon-money", label: "薪资计算器", route: "/salary-calculator", backgroundColor: "#afefbf", iconColor: "#44aa56"},
-                    {icon: "el-icon-notebook-2", label: "待办事项", route: "/todo-list", backgroundColor: "#ffee70", iconColor: "#bc7e21"},
+                    //{icon: "el-icon-money", label: "薪资计算器", route: "/salary-calculator", backgroundColor: "#afefbf", iconColor: "#44aa56"},
+                    //{icon: "el-icon-notebook-2", label: "待办事项", route: "/todo-list", backgroundColor: "#ffee70", iconColor: "#bc7e21"},
                     {icon: "el-icon-brush", label: "颜色转换", route: "/color-converter", backgroundColor: "#ffffef", iconColor: "#66665f"},
-                    {icon: "el-icon-coin", label: "记账本", route: "/color-converter", backgroundColor: "#ffffef", iconColor: "#66665f"},
+                    //{icon: "el-icon-coin", label: "记账本", route: "/color-converter", backgroundColor: "#ffffef", iconColor: "#66665f"},
                 ],
                 screenSize: "large"
             }
